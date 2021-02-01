1b542a0dcf46b447cfc68490918a72556ee24977
Henrique1818 <luiz.henrique01827@gmail.com>
2021-01-31 21:40:21 -0300

    criando a rota de criar atendimentos

--- controllers/atendimentos.js
+++ controllers/atendimentos.js
@@ -1,3 +1,9 @@
 module.exports = app => {
     app.get('/atendimentos', (req, res) => res.send('Rota de atendimentos'));
+
+    app.post('/atendimentos', (req, res) => {
+        console.log(req.body);
+
+        res.send('Rota de criação de atendimentos');
+    });
 }
