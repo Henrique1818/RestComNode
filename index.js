389c3b483703f98ff11ad9500cda5df397252ef9
Henrique1818 <luiz.henrique01827@gmail.com>
2021-01-31 21:38:24 -0300

    executando o banco de dados e o servidor

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
 const customExpress = require('./config/customExpress');
+const conexao = require('./infra/connection');
 
-const app = customExpress();
+conexao.connect(error => {
+    if(error) {
+        console.log(error);
+    } else {
+        console.log('conectado com sucesso!!');
 
-app.listen(3000, () => console.log('rodando na porta 3000'));
+        const app = customExpress();
+
+        app.listen(3000, () => console.log('rodando na porta 3000'));
+    }
+});
