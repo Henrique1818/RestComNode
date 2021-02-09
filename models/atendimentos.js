a9b2920e306fe34d1de77f4efc74295446f9798b
Henrique1818 <luiz.henrique01827@gmail.com>
2021-02-09 20:28:07 -0300

    :bug: tipo: fix - correcao do bug campos com nomes diferentes que existem na tabela

--- models/atendimentos.js
+++ models/atendimentos.js
@@ -1,10 +1,22 @@
+const moment = require('moment');
 const conexao = require('../infra/connection');
 
 class Atendimentos {
-    adiciona(atendimentos) {
+    adiciona(atendimento) {
+        const dataCriacao = moment().format('YYYY-MM-DD HH:MM:SS');
+        const data = moment(atendimento.data, 'DD/MM/YYYY').format('YYYY-MM-DD HH:MM:SS');
+
+        const atendimentoDatado = {
+            ...atendimento, 
+            dataCriacao, 
+            data
+        };
+
+        console.log(atendimentoDatado);
+
         const sql = `INSERT INTO Atendimentos SET ?`;
 
-        conexao.query(sql, atendimentos, (error, result) => {
+        conexao.query(sql, atendimentoDatado, (error, result) => {
             if(error) {
                 console.log(error);
             } else {
