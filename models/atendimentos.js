559015dbb8eb58d967d4070c3651518126b4a3c2
Henrique1818 <luiz.henrique01827@gmail.com>
2021-02-09 21:27:13 -0300

    :construction: Criando validacoes para a data e a quantidade minima de caracteres de nome, usando status code como resposta e erros customizados

--- models/atendimentos.js
+++ models/atendimentos.js
@@ -1,28 +1,50 @@
 const moment = require('moment');
+const atendimentos = require('../controllers/atendimentos');
 const conexao = require('../infra/connection');
 
 class Atendimentos {
-    adiciona(atendimento) {
+    adiciona(atendimento, res) {
         const dataCriacao = moment().format('YYYY-MM-DD HH:MM:SS');
         const data = moment(atendimento.data, 'DD/MM/YYYY').format('YYYY-MM-DD HH:MM:SS');
 
-        const atendimentoDatado = {
-            ...atendimento, 
-            dataCriacao, 
-            data
-        };
+        const dataEhValida = moment(data).isSameOrAfter(dataCriacao);
+        const clienteEhValido = atendimento.cliente.length >= 5;
 
-        console.log(atendimentoDatado);
-
-        const sql = `INSERT INTO Atendimentos SET ?`;
-
-        conexao.query(sql, atendimentoDatado, (error, result) => {
-            if(error) {
-                console.log(error);
-            } else {
-                console.log(result);
+        const validacoes = [
+            {
+                nome: 'data',
+                valido: dataEhValida,
+                mensagem: 'Data deve ser maior ou igual a data atual'
+            },
+            {
+                nome: 'cliente',
+                valido: clienteEhValido,
+                mensagem: 'Cliente deve ter pelo menos três caracteres'
             }
-        });
+        ];
+
+        const erros = validacoes.filter(campo => !campo.valido);
+        const existemErros = erros.length;
+
+        if(existemErros) {
+            res.status(400).json(erros);
+        } else {
+            const atendimentoDatado = {
+                ...atendimento, 
+                dataCriacao, 
+                data
+            };
+    
+            const sql = `INSERT INTO Atendimentos SET ?`;
+    
+            conexao.query(sql, atendimentoDatado, (error, result) => {
+                if(error) {
+                    res.status(400).json(error);
+                } else {
+                    res.status(201).json(result);
+                }
+            });
+        }
     }
 }
 
