0d182d024a79f5e45c72e1eab3ccef0f9ce94b8c
Henrique1818 <luiz.henrique01827@gmail.com>
2021-02-13 19:52:22 -0300

    refactor: validando tipos de imagens e tratando erros

--- files/uploadFiles.js
+++ files/uploadFiles.js
@@ -4,15 +4,17 @@ const path = require('path');
 module.exports = (caminho, nomeDoArquivo, callbackImagemCriada) => {
     const tiposValidos = ['jpg', 'png', 'jpeg'];
     const tipo = path.extname(caminho); 
-    const tipoEhValido = tiposValidos.indexOf(tipo.substring(1));
+    const tipoEhValido = tiposValidos.indexOf(tipo.substring(1)) === -1;
 
-    if(tipoEhValido === -1) {
-        console.error('Erro!, tipo inválido');
+    if(tipoEhValido) {
+        const erro = 'Tipo é inválido';
+        console.log(`Erro!, ${erro}`);
+        callbackImagemCriada(erro);
     } else {
         const novoCaminho = `./assets/imagens/${nomeDoArquivo}${tipo}`;
     
         fs.createReadStream(caminho)
             .pipe(fs.createWriteStream(novoCaminho))
-            .on('finish', () => callbackImagemCriada(novoCaminho));
+            .on('finish', () => callbackImagemCriada(false, novoCaminho));
     }
 }
