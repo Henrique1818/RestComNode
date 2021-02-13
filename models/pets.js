ef4a36dcc68f7562fed9777e72744a3896aeacaf
Henrique1818 <luiz.henrique01827@gmail.com>
2021-02-13 19:53:28 -0300

    refactor: tratando erros dependendo da extensão das imagens

--- models/pets.js
+++ models/pets.js
@@ -5,16 +5,20 @@ class Pet {
     adicionar(pet, res) {
         const query = 'INSERT INTO Pets SET ?';
 
-        uploadDeArquivos(pet.imagem, pet.nome, novoCaminho => {
-            const novoPet = { nome: pet.nome, imagem: novoCaminho };
-
-            conexao.query(query, novoPet, erro => {
-                if(erro) {
-                    res.status(400).json(erro);
-                } else {
-                    res.status(200).json(novoPet);
-                }
-            });
+        uploadDeArquivos(pet.imagem, pet.nome, (erro, novoCaminho) => {
+            if(erro) {
+                res.status(400).json({ erro });
+            } else {
+                const novoPet = { nome: pet.nome, imagem: novoCaminho };
+    
+                conexao.query(query, novoPet, erro => {
+                    if(erro) {
+                        res.status(400).json(erro);
+                    } else {
+                        res.status(200).json(novoPet);
+                    }
+                });
+            }
         });
 
     }
