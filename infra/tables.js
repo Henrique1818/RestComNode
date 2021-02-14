7e2ddf77c3a70837c5b8a7565ba72b9217cd5cfe
Henrique1818 <luiz.henrique01827@gmail.com>
2021-02-13 21:29:27 -0300

    refac: mudando o cliente do tipo varchar para 11, passando assim o cpf

--- infra/tables.js
+++ infra/tables.js
@@ -10,7 +10,7 @@ class Tabelas {
 
     criarAtendimentos() {
         const sql = `CREATE TABLE IF NOT EXISTS Atendimentos (
-            id int NOT NULL AUTO_INCREMENT, cliente varchar(50) NOT NULL, pet varchar(20),
+            id int NOT NULL AUTO_INCREMENT, cliente varchar(11) NOT NULL, pet varchar(20),
             servico varchar(20) NOT NULL, data datetime NOT NULL, dataCriacao datetime NOT NULL,
             status varchar(20) NOT NULL, observacoes text, PRIMARY KEY(id)
         )`;
